a7482510011848a5dff8cb2f69ce5a0ad4ee9fde
Peter Besze <pbesze@gmail.com>
2016-09-13 14:45:54 +0200

    Update TeXtService.java

--- src/main/java/base/TeXtService.java
+++ src/main/java/base/TeXtService.java
@@ -5,18 +5,12 @@ import java.util.List;
 import java.util.UUID;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-//import base.TeXt;
+
  
 @ManagedBean(name = "teXtService")
 @ApplicationScoped
 public class TeXtService {
-     
-    
-
-    //private final static String[] chaR;
-     
-
-     
+ 
     public List<TeXt> createTeXts(int maxsize) {
         List<TeXt> list = new ArrayList<TeXt>();
         for(int i = 2 ; i < maxsize ; i++) {
