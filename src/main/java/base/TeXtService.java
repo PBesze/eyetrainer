221f4a568692c14ec55ab2c4fc616d2268c5e855
Peter Besze <pbesze@gmail.com>
2016-09-14 10:25:53 +0200

    Update TeXtService.java

--- src/main/java/base/TeXtService.java
+++ src/main/java/base/TeXtService.java
@@ -16,7 +16,7 @@ public class TeXtService implements Serializable{
         List<TeXt> list = new ArrayList<TeXt>();
         for(int i = 1 ; i < maxsize+1 ; i++) {
          
-            list.add(new i, TeXt(getStyle(i), getRandomChaR(), getRandomChaR(), getRandomChaR(), getRandomChaR()));
+            list.add(new TeXt(getStyle(i), getRandomChaR(), getRandomChaR(), getRandomChaR(), getRandomChaR()));
         }
          
         return list;
