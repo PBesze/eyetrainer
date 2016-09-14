0243292700a77df5398791026fc27dc8364bab28
Peter Besze <pbesze@gmail.com>
2016-09-14 22:12:55 +0200

    Create taSk.java

---
+++ src/main/java/base/taSk.java
@@ -0,0 +1,23 @@
+package base;
+
+import java.io.Serializable;
+
+public class TaSk implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public String side;
+	
+    public TaSk(String  side) {
+		this.side = side;
+		
+	}
+	
+
+		public String getTaSk() {
+		return fontsizeStyle;
+	}
+
+	public void setTaSk(String taSk) {
+		this.taSk = taSk;
+	}
+
+}
