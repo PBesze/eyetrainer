337dd028fba57bdc14c9e60ddc834cd981876175
Peter Besze <pbesze@gmail.com>
2016-09-15 10:07:09 +0200

    Update TaSk.java

--- src/main/java/base/TaSk.java
+++ src/main/java/base/TaSk.java
@@ -9,7 +9,7 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class TaSk{
 public static int stepcounter = 0;
-    public int getSide() {
+    public String getSide() {
     	stepcounter++;
    	if (stepcounter%3 == 0 ) {return "Can you read it with your RIGHT EYE?";}
    	if (stepcounter%3 == 1 ) {return "Can you read it with your BOTH EYE?";}
