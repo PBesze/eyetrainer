d653ee84b7dda924eff0b25fb2e08791f335a505
Peter Besze <pbesze@gmail.com>
2016-09-15 10:09:12 +0200

    Update TaSk.java

--- src/main/java/base/TaSk.java
+++ src/main/java/base/TaSk.java
@@ -11,9 +11,9 @@ public class TaSk{
 public static int stepcounter = 0;
     public String getSide() {
     	stepcounter++;
-   	if (stepcounter%3 == 0 ) {return "Can you read it with your RIGHT EYE?";}
-   	if (stepcounter%3 == 1 ) {return "Can you read it with your BOTH EYE?";}
-   	else {return "Can you read it with your LEFT EYES?";}
+   	if (stepcounter%3 == 0 ) {return "Can you read it with your 2 EYES?";}
+   	if (stepcounter%3 == 1 ) {return "Can you read it with your LEFT EYE?";}
+   	else {return "Can you read it with your RIGHT EYE?";}
   
  
     }
